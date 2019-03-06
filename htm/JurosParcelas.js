12a2be77a090a048575da838b93ffe300f39b45a
Wagner Y <wagneryamuto@gmail.com>
2019-03-06 11:19:20 -0300

    Sent by DeployFC at 06/03/2019 11:19:20

---
+++ htm/JurosParcelas.js
@@ -0,0 +1,12 @@
+//Informe abaixo os juros para parcelamento em 1x, 2x, 3x, etc.
+var Juros=new Array();
+Juros[0]=0; //1x (à vista)
+Juros[1]=0; //2x
+Juros[2]=0; //3x
+Juros[3]=0; //4x
+Juros[4]=0; //5x
+Juros[5]=0; //6x
+Juros[6]=0; //7x
+Juros[7]=0; //8x
+Juros[8]=0; //9x
+Juros[9]=0; //10x
